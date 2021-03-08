5562208886e70195bbac81b7042239d4c782b870
Andres <andresrosalespinzon@gmail.com>
2021-03-08 23:16:37 +0100

    fixed wrong routing in users in navbar,,cards for profesionals, new search for nav and small edits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
 import { HeroComponent } from './components/hero/hero.component';
+
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ProfesionalFormularioComponent } from './components/profesionales/profesional-formulario/profesional-formulario.component';
@@ -10,21 +11,19 @@ import { UserFormularioComponent } from './components/users/user-formulario/user
 import { UsersComponent } from './components/users/users.component';
 
 const routes: Routes = [
-
-  { path: "", pathMatch: "full", redirectTo: 'home' },
-  { path: "home", component: HomeComponent },
-  { path: "home", component: HeroComponent },
-  { path: "freelance", component: UsersComponent },
-  { path: "form_freelance", component: UserFormularioComponent },
-  { path: "profesional", component: ProfesionalesComponent },
-  { path: "form_profesional", component: ProfesionalFormularioComponent },
-  { path: "contact", component: ContactComponent },
-  { path: "**", component: NotFoundComponent }
-
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HeroComponent },
+  { path: 'freelance', component: UsersComponent },
+  { path: 'form_freelance', component: UserFormularioComponent },
+  { path: 'profesional', component: ProfesionalesComponent },
+  { path: 'form_profesional', component: ProfesionalFormularioComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/services/profesionales.service.ts
+++ src/app/services/profesionales.service.ts
@@ -2,14 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProfesionalesService {
   baseUrl: string;
   arrProfesionales: any[];
 
   constructor(private httpClient: HttpClient) {
-    this.baseUrl = 'https://fakerapi.it/api/v1/companies?_quantity=100';
+    this.baseUrl = 'https://fakerapi.it/api/v1/companies?_quantity=10';
   }
   getAll() {
     return this.httpClient.get(this.baseUrl).toPromise();
