ae1227772104609c34fc5e6f5406331dfb9405a2
Samantha-Bitbucket <ottavianisamantha@gmail.com>
2021-03-08 20:15:17 +0100

    declarando los componentes en app.module + rellenando el nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,29 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfesionalesComponent } from './components/profesionales/profesionales.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProfesionalFormularioComponent } from './components/profesionales/profesional-formulario/profesional-formulario.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserFormularioComponent } from './components/users/user-formulario/user-formulario.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ContactComponent,
+    FooterComponent,
+    HeroComponent,
+    NavBarComponent,
+    NotFoundComponent,
     ProfesionalesComponent,
+    ProfesionalFormularioComponent,
+    UsersComponent,
+    UserFormularioComponent,
 
   ],
   imports: [
