7ee28b53061b7131be2d959ed9ab774dab40fb0e
Samantha-Bitbucket <ottavianisamantha@gmail.com>
2021-03-08 20:32:01 +0100

    inplementando los interfaces con ideas de informaciones

--- src/app/interfaces/profesional.ts
+++ src/app/interfaces/profesional.ts
@@ -1,2 +1,17 @@
 export interface Profesional {
+
+    /*     id: number;
+        email: string;
+        password: string;
+        confirm_password: string; 
+        name: string;
+        first_name: string;
+        name_company: string;
+        contact: number;
+        fiscal_id: string;
+        description_company: string;
+        location: string;
+        url: string;
+        video: string;
+        legal_conditions: boolean; */
 }
