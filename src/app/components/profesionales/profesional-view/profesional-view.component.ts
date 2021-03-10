dc04f30fb914b7c913a08242413acc2133b8e06d
Samantha-Bitbucket <ottavianisamantha@gmail.com>
2021-03-10 09:00:52 +0100

    creando el componente view del profesional para pintar 1 profesional.

---
+++ src/app/components/profesionales/profesional-view/profesional-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profesional-view',
+  templateUrl: './profesional-view.component.html',
+  styleUrls: ['./profesional-view.component.scss']
+})
+export class ProfesionalViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
