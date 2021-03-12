cd1ec250543a8f834df0e8e085ac5171658c78a7
Andres <andresrosalespinzon@gmail.com>
2021-03-12 11:34:48 +0100

    rebasing

--- src/app/components/users/user-formulario/user-formulario.component.ts
+++ src/app/components/users/user-formulario/user-formulario.component.ts
@@ -12,25 +12,14 @@ export class UserFormularioComponent implements OnInit {
   constructor(private modalService: NgbModal) {}
 
   ngOnInit(): void {}
-<<<<<<< Features
-
-  openXl(content) {
-    this.modalService.open(content, { size: 'xl' });
-  }
-=======
->>>>>>> changed the dropdowns from formulario to responsive nav
 
   // openXl(content) {
   //   this.modalService.open(content, { size: 'xl' });
   // }
 
-<<<<<<< Features
-  public isCollapsed = true;
-=======
   // openVerticallyCentered(content) {
   //   this.modalService.open(content, { centered: true });
   // }
 
   active = 1;
->>>>>>> changed the dropdowns from formulario to responsive nav
 }
